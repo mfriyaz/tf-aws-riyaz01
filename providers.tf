fc138f8398077bd942e49d51e1324a4c76bc699e
Riyaz <mfriyaz@gmail.com>
2024-06-12 11:05:29 +0800

    Commit 03

--- providers.tf
+++ providers.tf
@@ -10,9 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-southeast-1"
+  metadata_service_timeout = 5 # Optional, can be increased if there are timeouts
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
