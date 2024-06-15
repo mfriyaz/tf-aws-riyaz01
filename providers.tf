748d8527a0f35e880f14309f73ab9070b0134eb3
Riyaz <mfriyaz@gmail.com>
2024-06-15 20:07:33 +0800

    Commit 13

--- providers.tf
+++ providers.tf
@@ -12,7 +12,6 @@ provider "aws" {
   region = "ap-southeast-1"
   assume_role {
     role_arn     = "arn:aws:iam::757265768679:user/terraform-riyaz"
-    session_name = "terraform"
-  }
+      }
   }
 
